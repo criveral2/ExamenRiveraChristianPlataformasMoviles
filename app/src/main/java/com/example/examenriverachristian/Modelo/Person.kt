330fe22911151073c7a1b9d03b3ffa87917bf162
Christian <criveral2@est.ups.edu.ec>
2020-12-02 09:54:49 -0500

    Fin evaluacion 09:54

---
+++ app/src/main/java/com/example/examenriverachristian/Modelo/Person.kt
@@ -0,0 +1,25 @@
+package com.example.quesalga.Modelo
+
+import java.io.Serializable
+
+public class Person: Serializable {
+
+    var cedula: String ="Null"
+    var nombre: String = "Null"
+    var apellidos: String = "Null"
+    var edad: String = "Null"
+    var profecion: String = "Null"
+
+    var nivEstudios: String = "Null"
+    var otrosEstudios: String = "Null"
+    var idiomas: String = "Null"
+    var salud: String = "Null"
+    var telefono: String = "Null"
+    var familiar: String = "Null"
+
+    var nombOrganizacion: String = "Null"
+    var cargo: String = "Null"
+    var tipoInstitucion: String = "Null"
+    var codigoFormulario: String = "Null"
+
+}
